967c82a8197cd2bc5b58a3420a1ce072873ba691
Hao Fu <hfu11@binghamton.edu>
2019-05-09 12:33:29 -0400

    update materials

---
+++ test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('cascade.xml')
+
+img = cv2.imread('Emma_Thompson_0003.jpg')
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.45, 5)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 1)
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
